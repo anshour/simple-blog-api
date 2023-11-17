be61b196670a60e6aef6c8a70419a9d55bf8e826
Anshor J <lordlexa10@gmail.com>
2023-11-17 23:49:40 +0700

    Add docs link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Hello World</main>;
+  return (
+    <main className={styles.main}>
+      <div>
+        <h2>
+          Hello, for api docs see{" "}
+          <a href="https://github.com/anshour/simple-blog-api/blob/main/README.md">here</a>
+        </h2>
+      </div>
+    </main>
+  );
 }
