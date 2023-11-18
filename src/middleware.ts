775e61b3fa03de870f48e0c8c94cd09bd44857a6
Anshor J <lordlexa10@gmail.com>
2023-11-18 10:45:19 +0700

    Add patch allowed method

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
 
   // SET-UP CORS
   response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
   return response;
 }
