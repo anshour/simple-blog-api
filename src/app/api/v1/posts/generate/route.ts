1cefec3e5e065b2c6e08738e3632459196c03f01
Anshor J <lordlexa10@gmail.com>
2023-11-18 10:24:52 +0700

    Remove todo

--- src/app/api/v1/posts/generate/route.ts
+++ src/app/api/v1/posts/generate/route.ts
@@ -24,8 +24,6 @@ export async function GET(request: NextRequest) {
       author: data.author,
       preview: content.slice(0, 200).replace("\n", "").replace("\r", "") + "...",
       content,
-      //TODO: ADD PREVIEW
-      // preview: "",
     });
   }
 
