8fd6843e0a0e3631676af8e4a959d5acfc6ae497
Anshor J <lordlexa10@gmail.com>
2023-11-21 08:50:40 +0700

    Use insertMany intstead of loop insertone

--- src/app/api/v1/posts/seed/route.ts
+++ src/app/api/v1/posts/seed/route.ts
@@ -19,29 +19,32 @@ export async function GET(request: NextRequest) {
 
   const files = await fs.readdir("./src/seed/md", { withFileTypes: true });
 
+  const postsData = [];
   for (let index = 0; index < files.length; index++) {
     const filePath = path.join(files[index].path, files[index].name);
     const fullContent = await fs.readFile(filePath, "utf-8");
     const { data, content } = matter(fullContent);
 
-    await mongodbApi.insertOne({
-      collection: "posts",
-      document: {
-        title: data.title,
-        image: pickRandomImage(),
-        slug: slugify(data.title, { lower: true }),
-        tags: data.tags,
-        category: data.category,
-        author: data.author,
-        preview: content.slice(0, 200).replace("\n", "").replace("\r", "") + "...",
-        content,
-        created_at: {
-          $date: new Date().toISOString(),
-        },
-        updated_at: null,
+    postsData.push({
+      title: data.title,
+      image: pickRandomImage(),
+      slug: slugify(data.title, { lower: true }),
+      tags: data.tags,
+      category: data.category,
+      author: data.author,
+      preview: content.slice(0, 200).replace("\n", "").replace("\r", "") + "...",
+      content,
+      created_at: {
+        $date: new Date().toISOString(),
       },
+      updated_at: null,
     });
   }
+
+  await mongodbApi.insertMany({
+    collection: "posts",
+    documents: [...postsData],
+  });
 
   return NextResponse.json({ message: "Posts is successfully seeded" });
 }
